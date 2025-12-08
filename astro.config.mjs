95d050da17eb714642af9677d887fcb108c07570
Ori Mansur <orimansur@gmail.com>
2025-12-08 11:18:47 -0500

    Mobile Nav

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,20 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import sanity from '@sanity/astro';
+
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
     site: 'https://yorkgaragepros.com',
-    integrations: [sitemap()],
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    integrations: [sitemap(), sanity({
+        projectId: "xgztagdf",
+        dataset: "production",
+        useCdn: false, // for static builds
+    })],
+
+
 });
