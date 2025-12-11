63cf113ca5610dd3419b38ceb5abb7468755b1dd
Ori Mansur <orimansur@gmail.com>
2025-12-11 10:10:00 -0500

    tgm

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,21 +8,22 @@ import tailwindcss from '@tailwindcss/vite';
 import partytown from '@astrojs/partytown';
 
 
+
 export default defineConfig({
     site: 'https://yorkgaragepros.com',
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [sitemap(), partytown({
-        config: {
-            forward: ["dataLayer.push"],
-        }
-    }), sanity({
-        projectId: "xgztagdf",
-        dataset: "production",
-        useCdn: false, // for static builds
-    })],
+    integrations: [
+        sitemap(),
+        partytown({
+            config: {
+                forward: ["dataLayer.push"],
+            }
+        }), sanity({
+            projectId: "xgztagdf",
+            dataset: "production",
+            useCdn: false, // for static builds
+        })],
     output: "static",
-
-
 });
